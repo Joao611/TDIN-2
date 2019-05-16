bbb7c7bb733af88282c7ccb378605b19e272b720
Joao611 <joaofvde@gmail.com>
2019-05-16 23:55:45 +0100

    First commit

---
+++ StoreServer/IStoreService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace StoreServer {
+    [ServiceContract]
+    public interface IStoreService {
+        // updates store stock and prints receipt, bypasses order?
+        [OperationContract]
+        void SellBook(int id);
+
+        [OperationContract]
+        int GetBookStock(int id);
+
+        [OperationContract]
+        Order CreateOrder(int id);
+
+        [OperationContract]
+        Order SetState(int id);
+    }
+
+    public class Client {
+        private string email { get; }
+        private string name { get; }
+        private string address { get; }
+    }
+
+    /**
+     * Order for each book. Orders are always delivered at home, so there are no instant sells.
+     * An Order may need to wait for the warehouse to delivered the respective book title.
+     */ 
+    [DataContract]
+    public class Order {
+        public class State {
+            enum Type {
+                WAITING,
+                DISPATCH_OCCURS_AT,
+                DISPATCH_AT,
+            }
+            private Type type { get; }
+            private DateTime dispatchDate { get; }
+        }
+
+        [DataMember]
+        private string guid { get; }
+
+        [DataMember]
+        private string title { get; }
+
+        [DataMember]
+        private int quantity { get; }
+
+        [DataMember]
+        private Client client { get; }
+
+        [DataMember]
+        private int totalPrice { get; }
+
+        [DataMember]
+        private State state { get; }
+    }
+}
---
+++ StoreServer/StoreService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace StoreServer { 
+    public class StoreService : IStoreService {
+        readonly string database;
+
+        StoreService() {
+            string connection = ConfigurationManager.ConnectionStrings["StoreDB"].ConnectionString;
+            database = String.Format(connection, AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        public Order CreateOrder(int id) {
+            int id = 0;
+
+            using (SqlConnection c = new SqlConnection(database)) {
+                try {
+                    c.Open();
+                    string sql = "insert into TTickets(Author, Problem, Answer, Status) values (@a1, @p1, '', 1)"; // injection protection
+                    SqlCommand cmd = new SqlCommand(sql, c);                                                       // injection protection
+                    cmd.Parameters.AddWithValue("@a1", author);                                                    // injection protection
+                    cmd.Parameters.AddWithValue("@p1", problem);                                                   // injection protection
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = "select max(Id) from TTickets";
+                    id = (int)cmd.ExecuteScalar();
+                } catch (SqlException) {
+                } finally {
+                    c.Close();
+                }
+            }
+            return id;
+        }
+
+        public int GetBookStock(int id) {
+            throw new NotImplementedException();
+        }
+
+        public string SellBook(int value) {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public Order SetState(int id) {
+            throw new NotImplementedException();
+        }
+
+        void IStoreService.SellBook(int id) {
+            throw new NotImplementedException();
+        }
+    }
+}
