0dd66a69ccc539c770d8089678f6b6df4b6b2900
BeatrizBaldaia <bibibaldaia@hotmail.com>
2019-05-21 22:33:54 +0100

    get orders and books and set order state

--- TTService/StoreService.cs
+++ TTService/StoreService.cs
@@ -13,36 +13,114 @@ namespace StoreService {
             database = String.Format(connection, AppDomain.CurrentDomain.BaseDirectory);
         }
 
-        private Client GetClient(SqlConnection c, int id) {
-            string sql = "SELECT * FROM Clients" +
-                        " WHERE Id = @id";
-            SqlCommand cmd = new SqlCommand(sql, c);
-            cmd.Parameters.AddWithValue("@id", id);
-            using (SqlDataReader reader = cmd.ExecuteReader()) {
-                reader.Read();
-                return new Client() {
-                    id = id,
-                    name = reader["name"].ToString(),
-                    email = reader["email"].ToString(),
-                    address = reader["address"].ToString()
-                };
+        private Order.State getState(int stock, int quantity, bool instantSell) {
+            if (stock < quantity) {
+                return new Order.State() { type = Order.State.Type.WAITING };
+            } else {
+                if (instantSell) {
+                    return new Order.State() { type = Order.State.Type.DELIVERED, dispatchDate = DateTime.Now };
+                } else {
+                    return new Order.State() { type = Order.State.Type.DISPATCH_OCCURS_AT, dispatchDate = DateTime.Now.AddDays(1) };
+                }
             }
         }
 
-        private Book GetBook(SqlConnection c, int id) {
-            string sql = "SELECT * FROM Books" +
+        private Order.State getState(string stateType, DateTime date) {
+            switch (stateType) {
+                case "DELIVERED":
+                    return new Order.State() { type = Order.State.Type.DELIVERED, dispatchDate = date };
+                case "WAITING":
+                    return new Order.State() { type = Order.State.Type.WAITING, dispatchDate = date };
+                case "DISPATCH_OCCURS_AT":
+                    return new Order.State() { type = Order.State.Type.DISPATCH_OCCURS_AT, dispatchDate = date };
+                case "DISPATCHED_AT":
+                    return new Order.State() { type = Order.State.Type.DISPATCHED_AT, dispatchDate = date };
+                default:
+                    return null;
+            }
+        }
+
+        private Order.State nextState(string stateType) {
+            switch (stateType) {
+                case "WAITING":
+                    return new Order.State() { type = Order.State.Type.DISPATCH_OCCURS_AT, dispatchDate = DateTime.Now.AddDays(1) };
+                case "DISPATCH_OCCURS_AT":
+                    return new Order.State() { type = Order.State.Type.DISPATCHED_AT, dispatchDate = DateTime.Now.AddDays(2) };
+                default:
+                    return null;
+            }
+        }
+
+        private Order getOrder(SqlConnection c, int id) {
+            string sql = "SELECT * FROM Orders" +
                         " WHERE Id = @id";
             SqlCommand cmd = new SqlCommand(sql, c);
             cmd.Parameters.AddWithValue("@id", id);
             using (SqlDataReader reader = cmd.ExecuteReader()) {
                 reader.Read();
-                return new Book() {
-                    id = id,
-                    title = reader["title"].ToString(),
-                    stock = Convert.ToInt32(reader["stock"]),
-                    price = Convert.ToDouble(reader["price"])
-                };
+                Order order = new Order(GetClient(c, Convert.ToInt32(reader["Client"])),
+                    GetBook(c, Convert.ToInt32(reader["Book"])),
+                    Convert.ToInt32(reader["Quantity"]),
+                    getState(reader["State"].ToString(), reader.GetDateTime(6)));//is 6 the index of DispatchDate column???
+                reader.Close();
+                return order;
             }
+        }
+
+        private Client GetClient(SqlConnection c2, int id) {
+            Client client = null;
+            using (SqlConnection c = new SqlConnection(database)) {
+                try {
+                    c.Open();
+                    string sql = "SELECT * FROM Clients" +
+                        " WHERE Id = @id";
+                    SqlCommand cmd = new SqlCommand(sql, c);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    using (SqlDataReader reader = cmd.ExecuteReader()) {
+                        reader.Read();
+                        client = new Client() {
+                            id = id,
+                            name = reader["name"].ToString(),
+                            email = reader["email"].ToString(),
+                            address = reader["address"].ToString()
+                        };
+                        reader.Close();
+                    }
+                } catch (SqlException e) {
+                    Console.WriteLine("DB Exception: " + e);
+                } finally {
+                    c.Close();
+                }
+            }
+            return client;
+        }
+
+        private Book GetBook(SqlConnection c2, int id) {
+            Book book = null;
+            using (SqlConnection c = new SqlConnection(database)) {
+                try {
+                    c.Open();
+                    string sql = "SELECT * FROM Books" +
+                        " WHERE Id = @id";
+                    SqlCommand cmd = new SqlCommand(sql, c);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    using (SqlDataReader reader = cmd.ExecuteReader()) {
+                        reader.Read();
+                        book = new Book() {
+                            id = id,
+                            title = reader["title"].ToString(),
+                            stock = Convert.ToInt32(reader["stock"]),
+                            price = Convert.ToDouble(reader["price"])
+                        };
+                        reader.Close();
+                    }
+                } catch (SqlException e) {
+                    Console.WriteLine("DB Exception: " + e);
+                } finally {
+                    c.Close();
+                }
+            }
+            return book;
         }
 
         private void InsertOrderInDb(SqlConnection c, Order order) {
@@ -54,7 +132,7 @@ namespace StoreService {
             cmd.Parameters.AddWithValue("@b", order.book.id);
             cmd.Parameters.AddWithValue("@qty", order.quantity);
             cmd.Parameters.AddWithValue("@state", order.state.type.ToString());
-            cmd.Parameters.AddWithValue("@date", order.state.dispatchDate);
+            cmd.Parameters.AddWithValue("@date", order.state.dispatchDate.ToString("s"));
             cmd.ExecuteNonQuery();
         }
 
@@ -67,13 +145,23 @@ namespace StoreService {
             cmd.ExecuteNonQuery();
         }
 
+        private void UpdateOrderState(SqlConnection c, string id, Order.State state) {
+            string sql = "UPDATE Orders SET State = @s AND DispatchDate = @d" +
+                        " WHERE Id = @id";
+            SqlCommand cmd = new SqlCommand(sql, c);
+            cmd.Parameters.AddWithValue("@s", state.type.ToString());
+            cmd.Parameters.AddWithValue("@d", state.dispatchDate);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.ExecuteNonQuery();
+        }
+
         public Order CreateOrder(int clientId, int bookId, int quantity, bool instantSell) {
 
             using (SqlConnection c = new SqlConnection(database)) {
                 try {
                     c.Open();
-
-                    Order order = new Order(GetClient(c, clientId), GetBook(c, bookId), quantity, instantSell);
+                    Book book = GetBook(c, bookId);
+                    Order order = new Order(GetClient(c, clientId), book, quantity, getState(book.stock, quantity, instantSell));
                     InsertOrderInDb(c, order);
                     switch (order.state.type) {
                         case Order.State.Type.WAITING:
@@ -93,40 +181,85 @@ namespace StoreService {
                     return order;
                 } catch (SqlException e) {
                     Console.WriteLine("DB Exception: " + e);
+                } finally {
+                    c.Close();
                 }
             }
 
             return null;
         }
 
+        public List<Order> GetOrders() {
+            var orders = new List<Order>();
+            using (SqlConnection c = new SqlConnection(database)) {
+                try {
+                    c.Open();
+                    string sql = "SELECT * FROM Orders";
+                    SqlCommand cmd = new SqlCommand(sql, c);
+                    using (SqlDataReader reader = cmd.ExecuteReader()) {
+                        while (reader.Read()) {
+                            Order order = new Order(
+                                GetClient(c, Convert.ToInt32(reader["Client"])),
+                                GetBook(c, Convert.ToInt32(reader["Book"])),
+                                Convert.ToInt32(reader["Quantity"]),
+                                getState(reader["State"].ToString(), reader.GetDateTime(6))
+                            );
+                            orders.Add(order);
+                        }
+                        reader.Close();
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine("DB Exception: " + e);
+                } finally {
+                    c.Close();
+                }
+            }
+            return orders;
+        }
         public List<Book> GetBooks() {
             var books = new List<Book>();
             Console.WriteLine("-------------------------------------");
             using (SqlConnection c = new SqlConnection(database)) {
                 try {
-                c.Open();
-                string sql = "SELECT * FROM Books";
-                SqlCommand cmd = new SqlCommand(sql, c);
-                using (SqlDataReader reader = cmd.ExecuteReader()) {
-                    while (reader.Read()) {
-                        Book book = new Book() {
-                            id = Convert.ToInt32(reader["id"]),
-                            title = reader["title"].ToString(),
-                            stock = Convert.ToInt32(reader["stock"])
-                        };
-                        books.Add(book);
+                    c.Open();
+                    string sql = "SELECT * FROM Books";
+                    SqlCommand cmd = new SqlCommand(sql, c);
+                    using (SqlDataReader reader = cmd.ExecuteReader()) {
+                        while (reader.Read()) {
+                            Book book = new Book() {
+                                id = Convert.ToInt32(reader["id"]),
+                                title = reader["title"].ToString(),
+                                stock = Convert.ToInt32(reader["stock"])
+                            };
+                            books.Add(book);
+                        }
+                        reader.Close();
                     }
-                }
                 } catch (Exception e) {
                     Console.WriteLine("DB Exception: " + e);
+                } finally {
+                    c.Close();
                 }
             }
             return books;
         }
 
-        public Order SetState(string id) {
-            throw new NotImplementedException();
+        public Order SetState(string id, string stateType) {
+            using (SqlConnection c = new SqlConnection(database)) {
+                try {
+                    c.Open();
+                    Order.State state = nextState(stateType);
+                    UpdateOrderState(c, id, state);
+                    Order order = getOrder(c, Convert.ToInt32(id));
+                    return order;
+                } catch (SqlException e) {
+                    Console.WriteLine("DB Exception: " + e);
+                }
+                return null;
+            }
         }
+
+
 
         public void SellBook(int id, int bookId, int quantity, int clientId) {
             CreateOrder(clientId, bookId, quantity, true);
