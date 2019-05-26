ec78c61c3051f8d2309708f99929c5662b8c5580
BeatrizBaldaia <bibibaldaia@hotmail.com>
2019-05-26 05:04:03 +0100

    website layout

--- BookShopWebsite/src/components/BookCard.vue
+++ BookShopWebsite/src/components/BookCard.vue
@@ -2,9 +2,12 @@
     <router-link :to="`/book-pay/${book.id}`" tag="div" class="pointer">
         <div class="card">
             <div class="card-body">
+                <img :src="book.src">
                 <div class="card-title">{{book.title}}</div>
-                {{book.stock}}
-                €{{book.price}}
+                <div class="book-details">
+                    <!--<span>{{book.stock}}</span>-->
+                    <span>€{{book.price}}</span>
+                </div>
                 <!--<a :href="`/book-pay/${book.id}`" class="btn btn-primary">Checkout</a>-->
             </div>
         </div>
@@ -19,11 +22,27 @@
                 id: Number,
                 title: String,
                 stock: Number,
-                price: Number
+                price: Number,
+                src: String
             }
         }
     }
 </script>
 
 <style scoped>
+    div.card {
+        margin-top: 20px;
+    }
+    div.card-title {
+        font-weight: bold;
+    }
+    div.book-details {
+        display: flex;
+        justify-content: flex-end;
+    }
+    img {
+        height: 165px;
+        max-width: 140px;
+    }
+
 </style>
